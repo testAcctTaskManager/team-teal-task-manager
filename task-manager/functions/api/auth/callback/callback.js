91a41cf103aae5eb7611108e77a81145f5cb0776
mizotii <mztiiiii0@gmail.com>
2026-02-05 04:19:12 -0800

    fix linting

--- task-manager/functions/api/auth/callback/callback.js
+++ task-manager/functions/api/auth/callback/callback.js
@@ -18,9 +18,9 @@ export default async function onRequest({ request, env }) {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams({
             code,
-            client_id: import.meta.env.GOOGLE_CLIENT_ID,
-            client_secret: import.meta.env.GOOGLE_CLIENT_SECRET,
-            redirect_uri: import.meta.env.GOOGLE_REDIRECT_URI,
+            client_id: env.GOOGLE_CLIENT_ID,
+            client_secret: env.GOOGLE_CLIENT_SECRET,
+            redirect_uri: env.GOOGLE_REDIRECT_URI,
             grant_type: 'authorization_code'
         })
     });
