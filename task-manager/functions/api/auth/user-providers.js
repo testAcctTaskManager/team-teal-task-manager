b5d1d164dc0cb69f70dcc0c75eb641c5cc27be59
mizotii <mztiiiii0@gmail.com>
2026-02-05 04:05:50 -0800

    create basic endpoint

---
+++ task-manager/functions/api/auth/user-providers.js
@@ -0,0 +1,13 @@
+import { makeCrudHandlers } from "./helpers.js";
+
+const columnHandlers = makeCrudHandlers({
+  table: "User_Providers",
+  primaryKey: "id",
+  allowedColumns: ["id", "user_id", "provider", "access_token", "refresh_token", "token_expires_at", "created_at", "updated_at"],
+  dbEnvVar: "cf_db",
+  orderBy: "user_id",
+});
+
+export const onRequestGet = columnHandlers.collection;
+export const onRequestPost   = columnHandlers.collection;
+export const onRequestOptions = columnHandlers.collection;
