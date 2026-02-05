f19f1b4bf57bc73dd20fc9809acfcc8c0e10257f
mizotii <mztiiiii0@gmail.com>
2026-02-05 04:05:36 -0800

    add necessary query params to auth url

--- task-manager/src/components/login/LoginPage.jsx
+++ task-manager/src/components/login/LoginPage.jsx
@@ -2,7 +2,14 @@ import './Login.css';
 import { GoogleSignInButton } from './GoogleSignInButton';
 
 export default function LoginPage() {
-  const authURL = import.meta.env.VITE_GOOGLE_AUTH_URL
+  const authURLParams = new URLSearchParams({
+            client_id: import.meta.env.GOOGLE_CLIENT_ID,
+            redirect_uri: import.meta.env.GOOGLE_REDIRECT_URI,
+            response_type: 'code',
+            scope: 'openid email profile'
+        })
+  const authURL = new URL(import.meta.env.VITE_GOOGLE_AUTH_URL)
+  authURL.search = authURLParams.toString()
 
   return (
     <div
