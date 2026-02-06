9ee2ddc2aa83afb6538a493ad4532a141da1e05c
mizotii <mztiiiii0@gmail.com>
2026-02-05 17:36:49 -0800

    fix function export name

--- task-manager/functions/api/auth/callback.js
+++ task-manager/functions/api/auth/callback.js
@@ -1,4 +1,4 @@
-export default async function onRequest({ request, env }) {
+export async function onRequest({ request, env }) {
     const url = new URL(request.url);
     const code = url.searchParams.get('code');
     const error = url.searchParams.get('error');
