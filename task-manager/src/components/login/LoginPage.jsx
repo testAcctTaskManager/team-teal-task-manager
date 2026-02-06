bedeffb4f81064fed5a1ab8b0e348aa61a861c66
mizotii <mztiiiii0@gmail.com>
2026-02-05 17:07:32 -0800

    add super temporary bad fallback vars so i can just see if the auth url builds properly

--- task-manager/src/components/login/LoginPage.jsx
+++ task-manager/src/components/login/LoginPage.jsx
@@ -3,12 +3,13 @@ import { GoogleSignInButton } from './GoogleSignInButton';
 
 export default function LoginPage() {
   const authURLParams = new URLSearchParams({
-            client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-            redirect_uri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
+            // todo: remove fallbacks once implemented
+            client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID || "459719729310-mf1vkob8e7ljhcn44obrujolbsfpil27.apps.googleusercontent.com",
+            redirect_uri: import.meta.env.VITE_GOOGLE_REDIRECT_URI || "https://3d51cbf2.team-teal-task-manager.pages.dev/api/auth/callback",
             response_type: 'code',
             scope: 'openid email profile'
         })
-  const authURL = new URL(import.meta.env.VITE_GOOGLE_AUTH_URL)
+  const authURL = new URL(import.meta.env.VITE_GOOGLE_AUTH_URL || "https://accounts.google.com/o/oauth2/auth")
   authURL.search = authURLParams.toString()
 
   return (
