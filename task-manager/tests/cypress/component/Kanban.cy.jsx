cc954f73e4c499216b5ec5817be8475ca1440f64
HJStaiff <53165775+HJStaiff@users.noreply.github.com>
2026-02-12 17:22:14 -0800

    Create Kanban.cy.jsx

---
+++ task-manager/tests/cypress/component/Kanban.cy.jsx
@@ -0,0 +1,14 @@
+describe("Kanban component", () => {
+    
+    it("renders all five columns", () => {
+        
+    });
+
+    it("shows tasks in the correct column", () => {
+
+    });
+
+    it("updates displayed tasks based on filter correctly", () => {
+
+    });
+});
