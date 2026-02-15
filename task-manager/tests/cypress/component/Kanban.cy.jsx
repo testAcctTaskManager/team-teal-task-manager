37567152ff6f639655a40ecae002acf3b8ddf553
HJStaiff <53165775+HJStaiff@users.noreply.github.com>
2026-02-15 15:51:13 -0800

    Add test for tasks in correct column

--- task-manager/tests/cypress/component/Kanban.cy.jsx
+++ task-manager/tests/cypress/component/Kanban.cy.jsx
@@ -6,26 +6,36 @@ describe("Kanban component", () => {
     
     it("renders empty when there are no columns", () => {
         const columns = [];
-        cy.mount(<Kanban columns={columns}/>)
-        cy.get("body").contains("No Columns")
+        cy.mount(<Kanban columns={columns}/>);
+        cy.get("body").contains("No Columns").should("be.visible");
     });
 
     it("renders all five columns", () => {
 
         mountKanban();
 
-        cy.contains("To Do").should("be.visible")
-        cy.contains("Blocked").should("be.visible")
-        cy.contains("In Progress").should("be.visible")
-        cy.contains("In Review").should("be.visible")
-        cy.contains("Complete").should("be.visible")
+        cy.contains("To Do").should("be.visible");
+        cy.contains("Blocked").should("be.visible");
+        cy.contains("In Progress").should("be.visible");
+        cy.contains("In Review").should("be.visible");
+        cy.contains("Complete").should("be.visible");
     });
 
     it("shows tasks in the correct column", () => {
-        
+         mountKanban();
+         
+        cy.contains("To Do").parent().within(() => {
+            cy.contains("Set up project").should("exist");
+            cy.contains("Create tasks endpoint").should("exist");
+        });
+        cy.contains("Blocked").parent().within(() => {
+            cy.contains("Write docs").should("exist");
+        });
     });
 
-    it("updates displayed tasks based on filter correctly", () => {
-        
-    });
+    // TODO: implement task filtering.
+    //it("updates displayed tasks based on filter correctly", () => {
+    //    
+    //});
+    
 });
