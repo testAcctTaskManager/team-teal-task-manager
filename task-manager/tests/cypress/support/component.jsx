84c6c3dc3af55a400b366fa8d5180e9bada82686
KaylaCrush <kayla.e.rieck@gmail.com>
2026-02-26 11:15:47 -0800

    Fixed typo

--- task-manager/tests/cypress/support/component.jsx
+++ task-manager/tests/cypress/support/component.jsx
@@ -1,4 +1,4 @@
-g// Cypress component testing support file
+// Cypress component testing support file
 // - Re-exports the Cypress React mount function
 // - Sets up a helper to mount components with React Router
 /* eslint-disable react-refresh/only-export-components */
