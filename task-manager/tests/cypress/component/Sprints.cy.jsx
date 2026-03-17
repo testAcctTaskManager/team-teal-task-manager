82526eaab78baf832f0edde098c264bbd45cef45
HJStaiff <53165775+HJStaiff@users.noreply.github.com>
2026-03-16 17:27:21 -0700

    Create tasks rendering test

--- task-manager/tests/cypress/component/Sprints.cy.jsx
+++ task-manager/tests/cypress/component/Sprints.cy.jsx
@@ -1,8 +1,13 @@
 import Sprints from "../../../src/components/Sprints";
+import { mountSprints } from "../support/component";
 
 describe("Sprint component", () => {
 
     it("renders tasks for the sprint", () => {
+        mountSprints();
 
+        cy.contains("Sprints").should("be.visible");
+        cy.contains("Set up project").should("be.visible");
+        cy.contains("Create tasks endpoint").should("be.visible");
     });
 });
