2de54214428f8e52e959ae834fa49fcca38451ee
Aune Mitchell <auneemitchell@gmail.com>
2026-03-22 21:58:34 -0700

    Adding coverage requirements to vitest.config.mjs and adding coverage summary and artifact to the tests.yml github action

--- task-manager/vitest.config.mjs
+++ task-manager/vitest.config.mjs
@@ -12,6 +12,14 @@ export default defineConfig({
     include: ["tests/components/**/*.test.{js,ts,jsx,tsx}"],
     coverage: {
       provider: "v8",
+      reporter: ["text", "html", "lcov", "json-summary"],
+      reportsDirectory: "coverage/unit",
+      thresholds: {
+        lines: 75,
+        functions: 75,
+        branches: 75,
+        statements: 75,
+      },
       include: ["src/components/**/*.{js,jsx}", "src/pages/**/*.{js,jsx}", "src/contexts/**/*.{js,jsx}"],
       exclude: ["*ProjectSidebar", "*Board"],
     },
